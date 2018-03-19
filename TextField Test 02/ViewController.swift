478366d9b12adfbccd24f1c9ebe17e172b4bd67e
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-03-19 16:07:25 +0900

    text field text 02

--- TextField Test 02/ViewController.swift
+++ TextField Test 02/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // delegate 연결
+        myTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        myLabel.text = myTextField.text
+        myTextField.text = ""
+        
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        print("I pressed return Key")
+        //키패드내림
+        myTextField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
 
 }
 
